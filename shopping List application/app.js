ffdf44b63afa02244777e79eb26e3b32bf796ca1
Priyanshu <priyanshurazz4@gmail.com>
2023-07-21 23:14:30 +0530

    updation in second

--- shopping List application/app.js
+++ shopping List application/app.js
@@ -1,49 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toBuyList = document.getElementById("toBuyList");
-    const alreadyBoughtList = document.getElementById("alreadyBoughtList");
-    const nothingBoughtYet = document.getElementById("nothingBoughtYet");
+angular.module('ShoppingListApp', [])
+  .controller('ShoppingListController', ShoppingListController);
 
-    const shoppingItems = [
-        { name: "cookies", quantity: 10 },
-        { name: "milk", quantity: 2 },
-        { name: "bread", quantity: 3 },
-        { name: "eggs", quantity: 12 },
-        { name: "apples", quantity: 5 },
-    ];
+function ShoppingListController() {
+  var vm = this;
 
-    function displayToBuyList() {
-        shoppingItems.forEach((item) => {
-            const listItem = document.createElement("li");
-            listItem.textContent = `Buy ${item.quantity} ${item.name}`;
-            
-            const boughtButton = document.createElement("button");
-            boughtButton.textContent = "Bought";
-            boughtButton.addEventListener("click", function() {
-                buyItem(item);
-            });
+  // Initial To Buy list
+  vm.toBuyList = [
+    { name: "cookies", quantity: 10 },
+    { name: "apples", quantity: 5 },
+    { name: "milk", quantity: 2 },
+    { name: "bread", quantity: 3 },
+    { name: "eggs", quantity: 12 }
+  ];
 
-            listItem.appendChild(boughtButton);
-            toBuyList.appendChild(listItem);
-        });
+  // Initial Already Bought list
+  vm.alreadyBoughtList = [];
+
+  vm.buyItem = function(item) {
+    var index = vm.toBuyList.indexOf(item);
+    if (index !== -1) {
+      vm.toBuyList.splice(index, 1);
+      vm.alreadyBoughtList.push(item);
     }
-
-    function buyItem(item) {
-        const listItem = document.createElement("li");
-        listItem.textContent = `Bought ${item.quantity} ${item.name}`;
-        alreadyBoughtList.appendChild(listItem);
-        
-        const index = shoppingItems.indexOf(item);
-        shoppingItems.splice(index, 1);
-
-        toBuyList.innerHTML = "";
-        displayToBuyList();
-
-        if (shoppingItems.length === 0) {
-            nothingBoughtYet.style.display = "none";
-            toBuyList.style.display = "none";
-            alreadyBoughtList.innerHTML = "Everything is bought!";
-        }
-    }
-
-    displayToBuyList();
-});
+  };
+}
